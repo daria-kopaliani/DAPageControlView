64664f7011eff3ac38644d1278de9420515cecb4
Daria Kopaliani <daria.kopaliani@gmail.com>
2014-05-30 12:40:46 +0300

    Scroll disabled for `indicatorsView`

--- Example/Pods/DAPageControlView/Classes/DAPageControlView.m
+++ Example/Pods/DAPageControlView/Classes/DAPageControlView.m
@@ -48,7 +48,7 @@ static CGFloat const FCMaximumIndicatorViewWidth = 14.;
         self.indicatorsView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
         self.indicatorsView.dataSource = self;
         self.indicatorsView.delegate = self;
-        self.indicatorsView.scrollEnabled = YES;
+        self.indicatorsView.scrollEnabled = NO;
         self.indicatorsView.backgroundColor = [UIColor clearColor];
         [self addSubview:self.indicatorsView];
         
